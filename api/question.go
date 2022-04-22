bd2747a67a5d542aa440762e00043566d055ab20
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2022-04-22 12:25:40 +0200

    cleaning up handler stuff

--- api/question.go
+++ api/question.go
@@ -8,16 +8,33 @@ import (
 	"os"
 )
 
-// GetDailyQuestion fetches a daily question from the API
-func GetDailyQuestion(w http.ResponseWriter, r *http.Request) {
-	cfg := config.Firebase(
+var (
+	firebaseConfig = config.Firebase(
 		os.Getenv("FB_TYPE"),
 		os.Getenv("FB_PROJECT_ID"),
 		os.Getenv("FB_PRIVATE_KEY_ID"),
 		os.Getenv("FB_PRIVATE_KEY"),
 		os.Getenv("FB_CLIENT_EMAIL"),
 		os.Getenv("FB_CLIENT_ID"))
-	question, err := tapi.New(r.Context(), cfg).GetDailyQuestion()
+)
+
+// TriviaHandler is our main entry point for the Trivia API
+func TriviaHandler(w http.ResponseWriter, r *http.Request) {
+	triviaAPI := tapi.New(r.Context(), firebaseConfig)
+	switch r.Method {
+	case http.MethodGet:
+		handleGetDailyQuestion(w, triviaAPI)
+		break
+	case http.MethodPost:
+		handleSubmitAnswer(w, r, triviaAPI)
+		break
+	default:
+		respondWithStatus(w, http.StatusMethodNotAllowed)
+	}
+}
+
+func handleGetDailyQuestion(w http.ResponseWriter, api *tapi.TriviaAPI) {
+	question, err := api.GetDailyQuestion()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err)
 		return
@@ -25,10 +42,24 @@ func GetDailyQuestion(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, http.StatusOK, question)
 }
 
+func handleSubmitAnswer(w http.ResponseWriter, r *http.Request, api *tapi.TriviaAPI) {
+	result, err := api.SubmitAnswer(r.Body)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, result)
+}
+
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(&payload)
+}
+
+func respondWithStatus(w http.ResponseWriter, code int) {
+	w.WriteHeader(code)
+	w.Write([]byte(nil))
 }
 
 func respondWithError(w http.ResponseWriter, code int, err error) {
